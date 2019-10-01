a4ffcbb5673c7a606f7a5b546633f8c73b603102
Kup99 <46765138+Kup99@users.noreply.github.com>
2019-10-01 14:31:35 +0300

    Initial commit

---
+++ src/main/java/my/examples/com/logic/Main.java
@@ -0,0 +1,99 @@
+package my.examples.com.logic;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+import my.examples.com.model.User;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+//Остальное можно найти здесь:
+//https://vertex-academy.com/tutorials/ru/java-8-uchebnik/
+
+
+
+
+
+
+
+public class Main {
+    public static List<String> list = Arrays.asList("mr.White", "mr.Pink", "mr.Black");
+    public static List<Integer> intlist = Arrays.asList(1, 5, 8, 9);
+
+    public static void main(String[] args) {
+        Main main = new Main();
+       // main.demonstrateStreamPossibles();
+        //main.forEachDemonstrate();
+       // main.objectToString();
+       // main.streamToOtherCollection();
+    }
+
+    public void getListUsingStream() {
+
+
+    }
+
+    public void demonstrateStreamPossibles() {
+//найти любое значение
+        Optional<String> optional = list.stream().findAny();
+        System.out.println(optional);
+
+//если лист начинается с mr.P вывести
+        list.stream()
+                .filter((s -> s.startsWith("mr.P"))).
+                map(String::toUpperCase)
+                .sorted()
+                .forEach(System.out::println);
+//сделать первую букву заглавной
+        list.stream().map(StringUtils::capitalize).forEach(System.out::println);
+
+
+    }
+
+    public void forEachDemonstrate() {
+
+        //вывести все *2
+        intlist.stream().map(s -> s * 2).forEach(System.out::println);
+
+        //вывести все
+        intlist.stream().forEach(System.out::println);
+
+        //получить значения >=5 и не null
+        intlist.stream().filter(s->s>=5).filter(s -> s != null).forEach(System.out::println);
+    }
+
+
+    public void objectToString() {
+
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "Kup"));
+        users.add(new User(2, "guk"));
+        users.add(new User(3, "fas"));
+        users.add(new User(4, "down"));
+
+
+        //вывести getName у User
+        users.stream().map(User::getName).forEach(System.out::println);
+
+
+    }
+
+    public void streamToOtherCollection(){
+
+        //положить в лист
+        List<String>newList=list.stream().filter(s ->s.startsWith("mr.W")).collect(Collectors.toList());
+        System.out.println(newList);
+        //Лист в Сет
+        Set<String>newSet=list.stream().collect(Collectors.toSet());
+        newSet.stream().forEach(System.out::println);
+
+
+
+
+    }
+
+
+
+
+
+}
